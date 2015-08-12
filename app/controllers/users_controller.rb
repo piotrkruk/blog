d3e525cee07cec3d65dffb320d200faab550c0af
piotrkruk <pka.kruk@gmail.com>
2015-08-12 19:24:59 +0200

    Fixed a bug in authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
 
 	private
 		def user_params
-			params.require(:user).permit(:email, :password_hash, :password_salt, :password)
+			params.require(:user).permit(:email, :password_hash,
+				:password_salt, :password, :password_confirmation)
 		end
 end
