270789395ec0aca9c3523179472ddac1e529e436
piotrkruk <pka.kruk@gmail.com>
2015-08-16 18:06:27 +0200

    Created tag cloud feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 		gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
 		"http://gravatar.com/avatar/#{gravatar_id}.png"
 	end
+
+  def tag_cloud
+    Article.tag_counts.limit(60).sort_by(&:count).reverse
+  end
 end
